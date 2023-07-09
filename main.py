4f25556c7c88248ba6b12cbdf515d75e990b2e9f
Kuter Dinel <kuterdinel@gmail.com>
2023-07-09 06:10:13 +0300

    Add slide crawling & bug fixes

--- main.py
+++ main.py
@@ -44,33 +44,42 @@ parser.add_argument(
 )
 parser.add_argument(
     "--max-workers",
-    type=str,
+    type=int,
     default=4,  # A large number here may create too much load.
     help="Maximum number of worker threads to use.",
 )
+parser.add_argument("--crawl-slides", type=bool, default=True, help="Include Google Docs in crawl")
 
 args = parser.parse_args()
 
 threadpool = ThreadPoolExecutor(max_workers=args.max_workers)
 
 DOCS_BASE = "https://docs.google.com/document/d/"
+SLIDES_BASE = "https://docs.google.com/presentation/d/"
 DOCS_REGEX = f"^{DOCS_BASE}([^/#]*)"
+SLIDES_REGEX = f"^{SLIDES_BASE}([^/#]*)"
+
+SLIDES = "slides"
+DOCUMENT = "document"
 
 # To be able to reuse the connection.
 session = requests.Session()
 
 
-def html_url_from_id(did):
+def docs_html_from_id(did):
     """
     For a given Google docs document id get the url for html export.
     """
     return f"https://docs.google.com/feeds/download/documents/export/Export?id={did}&exportFormat=html"
 
+def slide_html_from_id(did):
+    return f"https://docs.google.com/presentation/d/{did}/htmlpresent"
 
 def un_google_url(url):
     """
     Remove the Google redirect.
     """
+    #FIXME: Make this more safu for against non redirect urls. 
     parsed = urlparse(url)
     if parsed.hostname != "www.google.com":
         return url
@@ -89,12 +98,27 @@ def document_id_from_url(url):
         return None
     return match.group(1)
 
+def slides_id_from_url(url):
+    """
+    Extract the document id from a document link.
+    """
+    match = re.match(SLIDES_REGEX, url)
+    if match == None:
+        return None
+    return match.group(1)
 
 def fetch_document_by_id(did):
     """
     Fetch the html contents of a document given a document id.
     """
-    response = session.get(html_url_from_id(did), allow_redirects=False)
+    response = session.get(docs_html_from_id(did), allow_redirects=False)
+    if response.status_code != 200:
+        return None
+
+    return response.text
+
+def fetch_slides_by_id(did):
+    response = session.get(slide_html_from_id(did), allow_redirects=False)
     if response.status_code != 200:
         return None
 
@@ -105,9 +129,19 @@ def find_document_title(dom):
     """
     Attempt to extract the document id from the document html dom.
     """
+    #NOTE: Do we want to have different handling for slides ?
     element = dom.find(class_="title")
     if element:
         return element.get_text()
+
+    # Should this be the default way of handling titles ?
+    element = dom.find("title")
+    if element:
+        title_raw = element.get_text()
+        match = re.match("(.*) - Google [^- ]*", title_raw)
+        if not match:
+            return title_raw
+        return match.group(1)
 
     if not args.allow_speculative_title_detection:
         return None
@@ -122,11 +156,10 @@ def find_document_title(dom):
 
     return None
 
-
 def title_slug(title):
     title = title.lower()
     title = title.replace(" ", "-")
-    return re.sub("[^" + string.ascii_letters + "-]", "", title)
+    return re.sub("[^" + string.ascii_letters + string.digits + "-]", "", title)
 
 
 def find_links(dom):
@@ -151,13 +184,36 @@ class Crawler:
         self.explored = set()
         self.results = []
         for seed in seeds:
-            did = document_id_from_url(seed)
-            self.to_explore.add(did)
+            item = self.item_from_url(seed)
+            if item:
+                self.to_explore.add(item)
 
-    def _single_fetch(self, did):
+    def item_from_url(self, url):
+        did = document_id_from_url(url)
+        if did:
+            return (DOCUMENT, did)
+        if args.crawl_slides:
+            did = slides_id_from_url(url)
+            if did:
+                return (SLIDES, did)
+        return None
+    
+    def item_to_url(self, item):
+        if item[0] == DOCUMENT:
+            return DOCS_BASE + item[1]
+        elif item[1] == SLIDES:
+            return SLIDES_BASE + item[1]
+        return None
+    
+    def _single_fetch(self, item):
         found = set()
         try:
-            contents = fetch_document_by_id(did)
+            contents = None
+            if item[0] == DOCUMENT:
+                contents = fetch_document_by_id(item[1])
+            elif item[0] == SLIDES:
+                contents = fetch_slides_by_id(item[1])
+
             if not contents:
                 # TODO: Maybe log something when this happens ?
                 return found
@@ -170,21 +226,21 @@ class Crawler:
 
             if args.download_folder:
                 result = os.path.join(
-                    args.download_folder, title_slug(title) + f"_{did}.html"
+                    args.download_folder, title_slug(title) + f"_{item[1]}.html"
                 )
                 file = open(result, "w")
                 file.write(contents)
                 file.close()
 
-            self.results.append((title, DOCS_BASE + did))
+            self.results.append((title, self.item_to_url(item)))
 
             links = find_links(dom)
             for link in links:
-                ldid = document_id_from_url(link)
-                if ldid and ldid not in self.explored:
-                    found.add(ldid)
+                item = self.item_from_url(link)
+                if item and item not in self.explored:
+                    found.add(item)
         except Exception as e:
-            print("Exception", e, "occured while processing", did, "skiping")
+            print("Exception", e, "occured while processing", item, "skiping")
         return found
 
     def expand(self):
@@ -194,9 +250,9 @@ class Crawler:
         print(len(self.to_explore), "documents to search")
         found = set()
         scrape_futures = []
-        for did in self.to_explore:
-            self.explored.add(did)
-            scrape_futures.append(threadpool.submit(self._single_fetch, did))
+        for item in self.to_explore:
+            self.explored.add(item)
+            scrape_futures.append(threadpool.submit(self._single_fetch, item))
 
         for future in scrape_futures:
             try:
